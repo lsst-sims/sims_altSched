e52d283635758fa2edc5226e264b78e516a3e8c7
Daniel R <drothchild@college.harvard.edu>
2017-06-16 10:33:55 -0400

    Added two time conversion functions to Utils

--- Utils.py
+++ Utils.py
@@ -21,6 +21,17 @@ def isRaInRange(ra, (minRa, maxRa)):
 def unix2mjd(timestamp):
     return timestamp / 86400 + 40587
 
+def mjd2unix(mjd):
+    return (mjd - 40587) * 86400
+
+def mjd2djd(mjd):
+    """
+    Convert Modified Julian Date to Dublin Julian Date (what pyephem uses).
+    (this function adapted from Peter Yoachim's code)
+    """
+    doff = 15019.5 # this equals ephem.Date(0)-ephem.Date('1858/11/17')
+    return mjd - doff
+
 def timetsamp(mjd):
     t = Time(mjd, format="mjd")
     return t.unix
